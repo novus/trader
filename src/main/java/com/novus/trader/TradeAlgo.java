59e04b60946af7feef6759cfa7957985e913d614
Chris Lewis <clewis@novus.com>
2012-10-11 15:38:39 -0400

    fresh lib

--- src/main/java/com/novus/trader/TradeAlgo.java
+++ src/main/java/com/novus/trader/TradeAlgo.java
@@ -3,7 +3,6 @@ package com.novus.trader;
 import com.novus.tradesim.*;
 import java.util.List;
 import java.util.Map;
-import java.util.LinkedList;
 
 public class TradeAlgo extends ManagedAlgo {
 	
